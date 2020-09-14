165d451a7ad9e5945f61eb34c39b503e0e92e41d
刘帅666 <862654857@qq.com>
2020-09-14 17:18:11 +0800

    完成todolist开发

---
+++ js/index.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', function() {
+    var vm = new Vue({
+        el: "#app",
+        data: {
+            todolist: [
+                {}
+            ],
+            optionList: ['全部事件', '未完成事件', '完成事件'],
+            option: '请选择事件类型',
+            inputValue: '',
+            reviseid: 0,
+            buttonname: '',
+            lists: [{ name: '未完成事件1', status: false }, { name: '未完成事件2', status: false }, { name: '未完成事件3', status: false }, { name: '完成事件1', status: true }, { name: '完成事件2', status: true }, { name: '完成事件3', status: true }],
+        },
+        methods: {
+            addlist() {
+                if (this.inputValue != '') {
+                    var obj = { name: this.inputValue, status: false }
+                    this.lists.push(obj)
+                    this.inputValue = ''
+                } else {
+                    alert('您输入的事件为空')
+                }
+            },
+            complete(index) {
+                this.lists[index].status = true
+
+            },
+            deleteid(index) {
+                this.lists.splice(index, 1)
+            },
+            revise(item, index) {
+                this.buttonname = '修改事件'
+                this.inputValue = item.name
+                this.reviseid = index
+            },
+            reviselist() {
+                this.lists[this.reviseid].name = this.inputValue
+                this.inputValue = ''
+                this.buttonname = ''
+            }
+        }
+    })
+})
