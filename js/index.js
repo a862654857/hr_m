62089edede9fe0e2cb4d40cba4508b400ed19cee
刘帅666 <862654857@qq.com>
2020-09-14 21:29:10 +0800

    完成组件化

--- js/index.js
+++ js/index.js
@@ -1,19 +1,46 @@
 window.addEventListener('load', function() {
-    var vm = new Vue({
-        el: "#app",
-        data: {
-            todolist: [
-                {}
-            ],
-            optionList: ['全部事件', '未完成事件', '完成事件'],
-            option: '请选择事件类型',
-            inputValue: '',
-            // 要修改的索引号
-            reviseid: 0,
-            // 按钮显示
-            buttonname: '',
-            lists: [{ name: '未完成事件1', status: false }, { name: '未完成事件2', status: false }, { name: '未完成事件3', status: false }, { name: '完成事件1', status: true }, { name: '完成事件2', status: true }, { name: '完成事件3', status: true }],
+    var zujian = {
+        data() {
+            return {
+                optionList: ['全部事件', '未完成事件', '完成事件'],
+                option: '请选择事件类型',
+                inputValue: '',
+                // 要修改的索引号
+                reviseid: 0,
+                // 按钮显示
+                buttonname: '',
+                lists: [{ name: '未完成事件1', status: false }, { name: '未完成事件2', status: false }, { name: '未完成事件3', status: false }, { name: '完成事件1', status: true }, { name: '完成事件2', status: true }, { name: '完成事件3', status: true }],
+            }
         },
+        template: `<div class="box">
+        <select v-model="option">
+        <option>请选择事件类型</option>
+        <option v-for="(item,i) in optionList">{{item}}</option>
+    </select>
+        <input type="text" v-model='inputValue'>
+        <button @click="reviselist" v-if="buttonname=='修改事件'">修改事件</button>
+        <button @click="addlist" v-else>添加事件</button>
+        <ul v-if="option=='全部事件'">
+            <li v-for="(item,i) in lists">
+                <span :class="item.status==true? 'xian':''">{{item.name}}</span>
+                <button @click='deleteid(i)'>删除</button>
+            </li>
+        </ul>
+        <ul v-if="option=='未完成事件'">
+            <li v-for="(item,i) in lists" v-if="item.status==false">
+                <span>{{item.name}}</span>
+                <button @click='complete(i)'>完成</button>
+                <button @click='deleteid(i)'>删除</button>
+                <button @click="revise(item,i)">修改</button>
+            </li>
+        </ul>
+        <ul v-if="option=='完成事件' ">
+            <li v-for="(item,i) in lists " v-if="item.status==true ">
+                <span class="xian ">{{item.name}}</span>
+                <button @click='deleteid(i)'>删除</button>
+            </li>
+        </ul>
+    </div>`,
         methods: {
             addlist() {
                 if (this.inputValue != '') {
@@ -41,6 +68,12 @@ window.addEventListener('load', function() {
                 this.inputValue = ''
                 this.buttonname = ''
             }
+        }
+    }
+    var vm = new Vue({
+        el: "#app",
+        components: {
+            'test': zujian
         }
     })
 })
